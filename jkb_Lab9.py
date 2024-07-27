8cafa381f37ac69f2615472bd2e4f67acb32a1b9
sofia23-a <sofia.ahumada@ufl.edu>
2024-07-27 09:56:42 -0400

    added decode function -sofia

--- jkb_Lab9.py
+++ jkb_Lab9.py
@@ -12,3 +12,16 @@ def encode(password): #function to encode password - Julia
 
     return new
 
+def decode(encoded_password): # function to decode password - Sofia
+    original = ""
+    for i in encoded_password:
+        if i == "0":
+            original += "7"
+        elif i == "1":
+            original += "8"
+        elif i == "2":
+            original += "9"
+        else:
+            original += str(int(i) - 3)
+    return original
+
